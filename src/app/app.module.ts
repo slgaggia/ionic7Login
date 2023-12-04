d718fd4a6138c53b9ea41f61d9d609b103eb8105
santiago.gaggia <santiago.gaggia@mseg.gba.gov.ar>
2023-12-04 12:28:54 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {AngularFireModule} from '@angular/fire/compat'
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDsT0iC5wRjJFuBI17YLepIdbuqmQt22fg",
+    authDomain: "fir-practice-aa1cd.firebaseapp.com",
+    projectId: "fir-practice-aa1cd",
+    storageBucket: "fir-practice-aa1cd.appspot.com",
+    messagingSenderId: "1045348752155",
+    appId: "1:1045348752155:web:b99f80240d1a9618d4d1be",
+    measurementId: "G-99SWM0LM0V"
+  }
 };
 
 /*
